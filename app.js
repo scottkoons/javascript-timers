80a4700f7883ee905fa92bb60715d5a270833705
scottkoons <scottkoons@gmail.com>
2020-09-01 18:42:52 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,31 @@
+//Countdown
+function countDown(num) {
+  let timer = setTimeout(function count() {
+    if (num > 0) {
+      console.log(num);
+      num--;
+      timer = setTimeout(count, 1000);
+    } else {
+      console.log("DONE!");
+    }
+  }, 1000);
+}
+countDown(4);
+// ------------
+
+//Random Game
+
+let i = 0;
+function randomGame() {
+  let num = Math.random();
+  if (num < 0.75) {
+    i++;
+    console.log(`The random number is: ${num} & i is: ${i}`);
+    setTimeout(randomGame, 1000);
+  } else {
+    console.log(`you went over .75 and i is: ${i}`);
+  }
+}
+
+randomGame();
+// -----------
